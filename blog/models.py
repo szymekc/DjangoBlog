df703a899921832bbbabb3c764e4edc6cc84eb69
Szymekc <szymekc98@gmail.com>
2019-11-05 17:27:56 +0100

    Added post list view

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class PostManager(models.Manager):
+    def createPost(self, author, title, text, date):
+        post = self.create(author,title,text,date)
+        return post
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    date = models.DateTimeField(default=timezone.now)
+    objects = PostManager()
+
+    def __str__(self):
+        return self.title
